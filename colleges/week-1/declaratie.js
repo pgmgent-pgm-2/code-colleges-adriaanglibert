ab5d007ba674feedeaf756edfc14de63f227e965
adriaan-glibert-development <info@adriaanglibert.be>
2023-11-20 11:09:22 +0100

    ✨ Declaratie van functies

---
+++ colleges/week-1/declaratie.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    return 'Hello';
+}
+
+const myArrowFunction = () => {
+    return 'Hello';
+}
+
+const myArrowFunctionThatReturns = () => (
+    'Hello'
+);
+
+const myArrowFunctionThatReturnsInOneLine = () => 'Hello';
+
+myArrowFunction();
+console.log(myArrowFunction());
