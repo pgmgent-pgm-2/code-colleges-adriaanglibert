4422db4122c4ea38fb554864a1946838738d8574
adriaan-glibert-development <info@adriaanglibert.be>
2023-12-04 14:31:24 +0100

    ✨ Oefeningen

--- oefeningen/week-2/async.js
+++ oefeningen/week-2/async.js
@@ -54,3 +54,40 @@ fetchDataWithPromise()
     .catch(error => {
         console.log('Fout bij ophalen van gegevens', error.message);
     })
+
+// Oefening 4
+function fetchUserData(userId) {
+    setTimeout(() => {
+        return new Promise((resolve, reject) => {
+            resolve({
+                id: userId,
+                firstName: 'Jan',
+                lastName: 'Modaal'
+            });
+        });
+    }, 1000)
+}
+
+function fetchUserPosts(user) {
+    return new Promise((resolve, reject) => {
+        resolve([{
+            userId: user.id,
+            title: 'Testpost',
+            body: 'Lorem ipsum.'
+        }, {
+            userId: user.id,
+            title: 'Testpost',
+            body: 'Lorem ipsum.'
+        }]);
+    });
+}
+
+async function getUserInfo(id) {
+    const user = await fetchUserData(id);
+    console.log();
+    const posts = await fetchUserPosts(user);
+    console.log(posts);
+}
+
+getUserInfo(3);
+
